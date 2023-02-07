e7715fa18d4ccdd624b2ef411206d8a38f73e68f
edwces <68016750+edwces@users.noreply.github.com>
2023-02-07 13:44:17 +0100

    detecting winner in match

--- lib/tic_tac_toe/game/match.ex
+++ lib/tic_tac_toe/game/match.ex
@@ -7,21 +7,71 @@ defmodule TicTacToe.Game.Match do
             board: nil,
             turn: nil,
             players: %{},
+            winner: nil,
             id: nil
 
   def new(id), do: %__MODULE__{board: Map.from_keys(Enum.to_list(0..8), nil), id: id}
 
   def is_open?(match), do: Enum.count(match.players) != 2
 
-  def move(%__MODULE__{turn: turn} = match, _pos, symbol) when symbol != turn, do: match
-  def move(match, pos, _symbol), do: do_move(match, pos)
+  def move(match, pos, symbol) do
+    with {:ok, _} <- check_turn(match, symbol),
+         {:ok, _} <- check_placement(match, pos) do
+      match
+      |> place_symbol(pos)
+      |> maybe_choose_winner()
+      |> change_turn()
+    else
+      _ -> match
+    end
+  end
 
-  defp do_move(match, pos) do
+  defp check_turn(%__MODULE__{turn: turn}, symbol) when symbol != turn,
+    do: {:error, "Incorrect turn"}
+
+  defp check_turn(match, _), do: {:ok, match}
+
+  defp check_placement(match, pos) do
+    case match.board[pos] do
+      nil -> {:ok, match}
+      _ -> {:error, "Already placed"}
+    end
+  end
+
+  defp place_symbol(match, pos) do
     update(%__MODULE__{
       match
-      | board: Map.put(match.board, pos, match.turn),
-        turn: opposite_symbol(match.turn)
+      | board: Map.put(match.board, pos, match.turn)
     })
+  end
+
+  defp change_turn(match) do
+    %__MODULE__{
+      match
+      | turn: opposite_symbol(match.turn)
+    }
+  end
+
+  defp maybe_choose_winner(%__MODULE__{turn: turn} = match) do
+    case match.board do
+      %{0 => ^turn, 1 => ^turn, 2 => ^turn} -> choose_winner(match, turn)
+      %{3 => ^turn, 4 => ^turn, 5 => ^turn} -> choose_winner(match, turn)
+      %{6 => ^turn, 7 => ^turn, 8 => ^turn} -> choose_winner(match, turn)
+      %{0 => ^turn, 3 => ^turn, 6 => ^turn} -> choose_winner(match, turn)
+      %{1 => ^turn, 4 => ^turn, 7 => ^turn} -> choose_winner(match, turn)
+      %{2 => ^turn, 5 => ^turn, 8 => ^turn} -> choose_winner(match, turn)
+      %{0 => ^turn, 4 => ^turn, 8 => ^turn} -> choose_winner(match, turn)
+      %{2 => ^turn, 4 => ^turn, 6 => ^turn} -> choose_winner(match, turn)
+      _ -> match
+    end
+  end
+
+  defp make_winner(match, symbol), do: %__MODULE__{match | winner: symbol}
+
+  defp choose_winner(match, symbol) do
+    match
+    |> make_winner(symbol)
+    |> stop()
   end
 
   def join(match, player) do
