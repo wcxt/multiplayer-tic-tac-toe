4462ab39b70a54bc3e4bcc6272067b9450083281
edwces <68016750+edwces@users.noreply.github.com>
2023-03-15 10:34:46 +0100

    change how room_id is generated

--- lib/tic_tac_toe/game/lobby.ex
+++ lib/tic_tac_toe/game/lobby.ex
@@ -3,10 +3,17 @@ defmodule TicTacToe.Game.Lobby do
   alias TicTacToe.Game.Server
 
   def create_server() do
-    case Supervisor.start_child(id: Ecto.UUID.generate()) do
+    case Supervisor.start_child(id: generate_room_id(6)) do
       {:ok, pid} -> pid
       {:error, {:already_started, pid}} -> pid
     end
+  end
+
+  def generate_room_id(length) do
+    length
+    |> :crypto.strong_rand_bytes()
+    |> Base.url_encode64(padding: false)
+    |> binary_part(0, length)
   end
 
   # FIXME: Inefficient use of selecting game servers
